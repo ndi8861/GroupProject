1e61b24d1a66d4f0892c933d8cd651ed1e3a1f63
moongs <89570502+moongs95@users.noreply.github.com>
2024-01-20 03:23:53 +0900

    done flex
    done flex

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const toggleBtn = document.querySelector('.nav-toggleBtn');
 const rmenu = document.querySelector('.nav-rmenu');
-const lmenu = document.querySelector('.nav-lmenu');
 
 toggleBtn.addEventListener('click', () => {
   rmenu.classList.toggle('active')
-  lmenu.classList.toggle('active')
 })
